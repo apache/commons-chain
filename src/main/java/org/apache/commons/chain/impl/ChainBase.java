26e42a6ac31bf37d009e08d54756569155cfb87e
Simone Tripodi <simonetripodi@apache.org>
2011-08-31 20:22:26 +0000

    fixed checkstyle violation: Unused import - java.util.Iterator
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/chain/branches/version-2.0-work@1163796 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/chain/impl/ChainBase.java
+++ src/main/java/org/apache/commons/chain/impl/ChainBase.java
@@ -19,7 +19,6 @@ package org.apache.commons.chain.impl;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.chain.Chain;
