e9718c2a7298c27593b997616f1a679e13f7eddf
Simone Tripodi <simonetripodi@apache.org>
2011-09-04 16:16:11 +0000

    extending LookupCommand, ServletPathMapper can be assigned to Command
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/chain/branches/version-2.0-work@1165065 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/chain/web/servlet/ServletPathMapper.java
+++ src/main/java/org/apache/commons/chain/web/servlet/ServletPathMapper.java
@@ -37,7 +37,7 @@ import org.apache.commons.chain.generic.LookupCommand;
  * @author Craig R. McClanahan
  */
 
-public class ServletPathMapper<C extends Context> extends LookupCommand<C> implements Command<C> {
+public class ServletPathMapper<C extends Context> extends LookupCommand<C> {
 
 
     // ------------------------------------------------------ Instance Variables
