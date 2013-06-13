2acbd2f5394b9b00567e77459ba7edaf3b46dd03
Benedikt Ritter <britter@apache.org>
2013-06-13 18:51:35 +0000

    Don't use wildcard imports
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/chain/trunk@1492817 13f79535-47bb-0310-9956-ffa450edef68

--- configuration/xml/src/test/java/org/apache/commons/chain2/config/xml/XmlConfigParser2TestCase.java
+++ configuration/xml/src/test/java/org/apache/commons/chain2/config/xml/XmlConfigParser2TestCase.java
@@ -17,18 +17,31 @@
 
 package org.apache.commons.chain2.config.xml;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Iterator;
+
 import org.apache.commons.chain2.Catalog;
 import org.apache.commons.chain2.CatalogFactory;
 import org.apache.commons.chain2.Context;
-import org.apache.commons.chain2.impl.*;
+import org.apache.commons.chain2.impl.AddingCommand;
+import org.apache.commons.chain2.impl.CatalogBase;
+import org.apache.commons.chain2.impl.CatalogFactoryBase;
+import org.apache.commons.chain2.impl.ChainBase;
+import org.apache.commons.chain2.impl.ContextBase;
+import org.apache.commons.chain2.impl.DelegatingCommand;
+import org.apache.commons.chain2.impl.DelegatingFilter;
+import org.apache.commons.chain2.impl.ExceptionCommand;
+import org.apache.commons.chain2.impl.ExceptionFilter;
+import org.apache.commons.chain2.impl.NonDelegatingCommand;
+import org.apache.commons.chain2.impl.NonDelegatingFilter;
 import org.apache.commons.digester3.Digester;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.Iterator;
-
-import static org.junit.Assert.*;
 
 
 /**
--- configuration/xml/src/test/java/org/apache/commons/chain2/config/xml/XmlConfigParserTestCase.java
+++ configuration/xml/src/test/java/org/apache/commons/chain2/config/xml/XmlConfigParserTestCase.java
@@ -17,21 +17,32 @@
 
 package org.apache.commons.chain2.config.xml;
 
-import org.apache.commons.chain2.Catalog;
-import org.apache.commons.chain2.CatalogFactory;
-import org.apache.commons.chain2.Context;
-import org.apache.commons.chain2.config.xml.ConfigRuleSet;
-import org.apache.commons.chain2.config.xml.XmlConfigParser;
-import org.apache.commons.chain2.impl.*;
-import org.apache.commons.digester3.Digester;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.net.URL;
 import java.util.Iterator;
 
-import static org.junit.Assert.*;
+import org.apache.commons.chain2.Catalog;
+import org.apache.commons.chain2.CatalogFactory;
+import org.apache.commons.chain2.Context;
+import org.apache.commons.chain2.impl.AddingCommand;
+import org.apache.commons.chain2.impl.CatalogBase;
+import org.apache.commons.chain2.impl.CatalogFactoryBase;
+import org.apache.commons.chain2.impl.ChainBase;
+import org.apache.commons.chain2.impl.ContextBase;
+import org.apache.commons.chain2.impl.DelegatingCommand;
+import org.apache.commons.chain2.impl.DelegatingFilter;
+import org.apache.commons.chain2.impl.ExceptionCommand;
+import org.apache.commons.chain2.impl.ExceptionFilter;
+import org.apache.commons.chain2.impl.NonDelegatingCommand;
+import org.apache.commons.chain2.impl.NonDelegatingFilter;
+import org.apache.commons.digester3.Digester;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 
 /**
