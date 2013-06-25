340ad1d60117da6e22d23e787ed65135c6f9840f
Benedikt Ritter <britter@apache.org>
2013-06-25 19:26:13 +0000

    Add test for NonDelegatingCommand
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/chain/trunk@1496599 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test-utils/src/test/java/org/apache/commons/chain2/testutils/NonDelegatingCommandTestCase.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.chain2.testutils;
+
+import static org.apache.commons.chain2.testutils.HasLog.hasLog;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.collection.IsMapContaining.hasKey;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.apache.commons.chain2.Context;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @version $Id$
+ */
+public class NonDelegatingCommandTestCase {
+
+    private static final String ID = UUID.randomUUID().toString();
+
+    private NonDelegatingCommand command;
+    private Context<String, Object> context;
+
+    @Before
+    public void setUp() throws Exception {
+        command = new NonDelegatingCommand(ID);
+        context = new TestContext<String, Object>();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void executeNullThrowsException() throws Exception {
+        command.execute(null);
+    }
+
+    @Test
+    public void createsLogInEmptyContext() throws Exception {
+        command.execute(context);
+
+        assertThat(context.keySet(), hasSize(1));
+        assertThat(context, hasKey("log"));
+        assertThat(context, hasLog(ID));
+    }
+
+    @Test
+    public void existingLogIsReused() throws Exception {
+        context.put("log", new StringBuilder("some content"));
+        command.execute(context);
+
+        assertThat(context, hasLog("some content/" + ID));
+    }
+}
